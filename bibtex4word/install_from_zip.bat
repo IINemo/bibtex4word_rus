392fccc24934a2d950b9c818af1508b6e6726743
IINemo <ArtemShelmanov@gmail.com>
2021-02-14 14:07:58 +0300

    Update install_from_zip.bat

--- bibtex4word/install_from_zip.bat
+++ bibtex4word/install_from_zip.bat
@@ -4,6 +4,7 @@ set script_dir=%~dp0
 
 set INSTALL_DIR="%AppData%\Microsoft\Word\STARTUP"
 "%script_dir%\unzip.vbs" "%script_dir%\bibtex4word.zip" "%script_dir%\bibtex4word"
+mkdir "%INSTALL_DIR%"
 copy "%script_dir%\bibtex4word\bibtex4word.dot" "%INSTALL_DIR%"
 
 ENDLOCAL
